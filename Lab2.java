2c1c2fb1fadc6acf1c83114cc9034be07066faaa
Nick <njw26@pitt.edu>
2015-09-17 13:45:07 -0400

    updated

--- Lab2.java
+++ Lab2.java
@@ -38,7 +38,7 @@ public class Lab2 {
 	    // all of the wives, sacks, cats, and kittens are as well, along
 	    // with the narrator.
 	    // Calculate the number going to St. Ives
-	    total = 2 + numWives + numSacksPerWife + numCatsPerSack + numKitsPerCat;
+	    total = 2 + numWives * numSacksPerWife * numCatsPerSack * numKitsPerCat;
 	} else {
 	    // Otherwise, only the narrator is going!
 	    total = 1;
